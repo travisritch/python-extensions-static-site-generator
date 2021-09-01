f836a54bfa87c906810fb3d0235074d66eec8b50
Travis Ritch <travisritch@gmail.com>
2021-09-01 05:51:07 -0600

    Fixing errors in menu.py

--- ssg/extensions/menu.py
+++ ssg/extensions/menu.py
@@ -6,15 +6,17 @@ files = []
 @hooks.register("collect_files")
 def collect_files(source, site_parsers):
     valid = lambda p : not isinstance(p, parsers.ResourceParser)
-    for parser in list(filter(valid, site_parsers)):
-        if parser.valid_file_ext(path.suffix):
-            files.append(path)
+    for path in source.rglob("*"):
+        for parser in list(filter(valid, site_parsers)):
+            if parser.valid_file_ext(path.suffix):
+                files.append(path)
+
 
 @hooks.register("generate_menu")
 def generate_menu(html, ext):
     template = '<li><a href="{}{}">{}</a></li>'
     menu_item = lambda name, ext : template.format(name, ext, name.title())
-    menu = ["\n".join(menu_item(path.stem, ext) for path in files)]
+    menu = "\n".join([menu_item(path.stem, ext) for path in files])
     
     return "<ul>\n{}<ul>\n{}".format(menu, html)
 
