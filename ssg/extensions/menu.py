63895330226640c49b6e78b661bccf62d54af87f
Travis Ritch <travisritch@gmail.com>
2021-09-01 05:56:17 -0600

    Stupid Pluralsight error in returned string for generate_menu

--- ssg/extensions/menu.py
+++ ssg/extensions/menu.py
@@ -17,6 +17,5 @@ def generate_menu(html, ext):
     template = '<li><a href="{}{}">{}</a></li>'
     menu_item = lambda name, ext : template.format(name, ext, name.title())
     menu = "\n".join([menu_item(path.stem, ext) for path in files])
-    
-    return "<ul>\n{}<ul>\n{}".format(menu, html)
+    return "<ul>\n{}</ul>\n{}".format(menu, html)
 
