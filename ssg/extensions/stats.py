67ffc8d8846b748f13cfc293da18c6e70a27eeaa
Travis Ritch <travisritch@gmail.com>
2021-09-01 06:08:44 -0600

    Finishing up to 5 for module4

---
+++ ssg/extensions/stats.py
@@ -0,0 +1,20 @@
+from ssg import hooks
+import time
+
+start_time = None
+total_written = 0
+
+@hooks.register("start_build")
+def start_build():
+    global start_time
+    start_time = time.localtime()
+
+@hooks.register("written")
+def written():
+    global total_written
+    total_written += 1
+
+@hooks.register("stats")
+def stats():
+    final_time = time.localtime() - start_time
+    average = final_time / total_written if total_written != 0 else 0
